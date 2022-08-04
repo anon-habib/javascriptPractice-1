761ed776ea2e3b6bf65be30b54a9312957ea13d0
Habibur Rahman Ove <35954652+anon-habib@users.noreply.github.com>
2022-08-04 13:43:18 -0700

    Update isLeapYear.js

--- isLeapYear.js
+++ isLeapYear.js
@@ -6,7 +6,20 @@ function isLeapYear(number){
     }
     return false;
 }
-const myYear = isLeapYear(1952); // input filed
+const myYear = isLeapYear(1952); // input field
 console.log(myYear);
 const yourYear = isLeapYear(2022); // input field
 console.log(yourYear);
+
+// other complex, returs true / false.
+function isLeapYear2(year2){
+    const reminder = year2 % 4;
+    if((0 == year2 % 4) && (0 != year2 % 100) || (0 == year2 % 400)){
+        return true;
+    }
+    return false;
+}
+const myYear2 = isLeapYear2(1952); // input field
+console.log(myYear2);
+const yourYear2 = isLeapYear2(2022); // input field
+console.log(yourYear2);
